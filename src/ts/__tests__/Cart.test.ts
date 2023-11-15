b50e2e1851c8710b7f92dac57c76554e8b3e3b78
Paul Solovey <p.v.solovey@ya.ru>
2023-11-15 17:49:41 +0300

    Task 1 is done

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,25 @@
 import Cart from '../service/Cart';
+import Book from '../domain/Book';
+import MusicAlbum from '../domain/MusicAlbum';
+import Movie from '../domain/Movie';
 
-test('new card should be empty', () => {
-  const cart = new Cart();
+const cart = new Cart();
 
+test('new cart should be empty', () => {
   expect(cart.items.length).toBe(0);
+});
+
+test('add a Book to the cart', () => {
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  expect(cart.items.length).toBe(1);
+});
+
+test('add a MusicAlbum to the cart', () => {
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  expect(cart.items.length).toBe(2);
+});
+
+test('add a Movie to the cart', () => {
+  cart.add(new Movie(1010, 'The Avengers', 800, 2012, 'USA', 'Avangers Assemble', 'sci-fi, action, fantasy, adventure', 137));
+  expect(cart.items.length).toBe(3);
 });
